11490616f08fe1d6fc66ebfcce5369e22f98fc9a
SeaSnake10 <76809064+SeaSnake10@users.noreply.github.com>
2021-05-22 16:58:23 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
 var pilot, ducks, b, pImg, dImg, bg, play, r, y, g;
 var yImg, gImg;
-var health = 100;
+var health = 1;
 var score = 0;
+var HighScore;
 var gameState = 0;
+var database;
 
 function preload(){
   pImg = loadImage("img/pilot1.5.png");
   dImg = loadImage("img/duck.png");
   bg = loadImage("img/sky.jpg");
-  yImg = loadImage("img/pilot2.5.png");
-  gImg = loadImage("img/pilot 3.5.png")
 }
 
 function setup() {
   createCanvas(displayWidth,displayHeight);
   pilot = createSprite(displayWidth/2, 400, 50, 50);
   pilot.addImage("air", pImg);
-  w=random(100,950)
+  w=random(10,1000)
   play = createButton("Play")
   b = new Group();
 }
@@ -28,23 +28,19 @@ function draw() {
   fill("red");
   text("Plane Integrity: "+health,100,100);
   text("Score: "+score, 100,150);
+  text("High Score"+HighScore, 100, 200);
   pilot.debug = false;
   pilot.setCollider("rectangle", 0,0,160,140)
 
   if(gameState === 0){ 
-    Customization();
-    if(play.mousePressed()){
+    text("Press Space to Play", displayWidth/2-80, 300)
+    if(keyDown("SPACE")){
       gameState = 1;
     }
   }
 
   if(gameState === 1){
-  score = score+ Math.round(getFrameRate()/10);
-
-  r.hide();
-  y.hide();
-  g.hide();
-  play.hide();
+  score = score+ Math.round(frameCount/150);
  
   if(pilot.isTouching(b)){
    health=health-1;
@@ -53,10 +49,14 @@ function draw() {
   if(health<1){
     gameState = 2;
   }
+  if(score>HighScore){
+    HighScore = score;
+    update();
+  }
 
   WATERFOWL();
   movement();
-} 
+  } 
 
   if(gameState === 2){
     pilot.velocityX = 0;
@@ -64,7 +64,7 @@ function draw() {
     b.destroyEach();
     textSize(20)
     fill("black");
-    text("Your Plane Has Crashed Due Too Much Damage", displayWidth/2-150, displayHeight/2-50)
+    text("Your Plane Has Crashed Due To Too Much Damage", displayWidth/2-150, displayHeight/2-50)
     textSize(20)
     fill(249, 49, 36)
     text("Game Over", displayWidth/2-10, displayHeight/2)
@@ -81,13 +81,13 @@ function movement(){
     pilot.velocityX=0;
   }
   if(keyWentDown(UP_ARROW)){
-    pilot.velocityY=-3;
+    pilot.velocityY=-5;
   } 
   if(keyWentUp(UP_ARROW)){
     pilot.velocityY=0;
   }
   if(keyWentDown(DOWN_ARROW)){
-    pilot.velocityY=+3;
+    pilot.velocityY=+5;
   } 
   if(keyWentUp(DOWN_ARROW)){
     pilot.velocityY=0;
@@ -101,7 +101,7 @@ function movement(){
 }
 
 function WATERFOWL() {
-  if(frameCount%200 === 0) {
+  if(frameCount%150 === 0) {
     for(i=0;i<5;i++)
   {
     w=random(200,950);
@@ -116,23 +116,8 @@ function WATERFOWL() {
   }
 }
 
-function Customization(){
-r = createButton("Red");
-r.position(100,200);
-y = createButton("Yellow");
-y.position(100,250);
-g = createButton("Green");
-g.position(100,300);
-
-if(y.mousePressed()){
-  pilot.addImage("variant", yImg);
-}
-
-if(r.mousePressed()){
-  pilot.addImage("variant", pImg);
-}
-
-if(g.mousePressed()){
-  pilot.addImage("variant2", gImg)
-}
+update(){
+  database.ref('/').update({
+    HighScore: HighScore
+  });
 }
